ba0a6b8d729b879064b3b8868876feef0b6048e3
hokuseihal <hokuseihal@gmail.com>
2021-05-12 17:44:37 +0900

    rm save img

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def operate():
         writer.add_scalars('stats',stats,i)
         generatedimages = (model.generator(testinput) * 0.5) + 0.5
         writer.add_images('images', generatedimages, i)
-        save_image(generatedimages, f'{savefolder}/{i}.jpg')
+        # save_image(generatedimages, f'{savefolder}/{i}.jpg')
         if i % 1000 == 0 and i!=0:
             # get FID
             fid = U.fid(realsigma, realmu, *fakemvci.get(isbias=True))
