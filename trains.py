e3ff2dce3e977bfe55f1ec091abdf939fb32dc74
hokuseihal <hokuseihal@gmail.com>
2021-05-19 10:35:11 +0900

    add trains with settings

---
+++ trains.py
@@ -0,0 +1,14 @@
+from setting import setting
+import subprocess
+def makecommand(obj,c):
+    if obj==[]:
+        cmd=c+['--savefolder',"".join(c[2:]).replace('--','')]
+        print('\033[32m'+f'{" ".join(c)}'+'\033[0m')
+        subprocess.run(cmd)
+        return 0
+    arg,values=obj[0]
+    for v in values:
+        makecommand(obj[1:],c+[f'--{arg}', f'{v}'])
+        
+print('s',setting)
+makecommand(setting,['python3','main.py'])
