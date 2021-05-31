853104cf89dce3fa2ab5925f58d1db592fe372d9
hokuseihal <hokuseihal@gmail.com>
2021-05-31 14:36:59 +0900

    add pixelcnn (maybe including many bugs)

---
+++ model/pixelcnn.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MaskedConv2D(nn.Module):
+    def __init__(self, in_ch, out_ch, k):
+        super(MaskedConv2D, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, k)
+        mask = torch.zeros(in_ch, k, k)
+        mask[:, :k // 2, :] = 1
+        mask[:, k // 2, :k // 2] = 1
+        self.mask=nn.Parameter(mask)
+        self.k = k
+
+    def forward(self, x):
+        x = F.pad(x, [(self.k - 1) // 2] * 4)
+        return F.conv2d(x, self.conv.weight * self.mask, self.conv.bias)
+
+
+class MaskedAtention(nn.Module):
+    def __init__(self, feature, num_heads=1):
+        super(MaskedAtention, self).__init__()
+        self.attn = nn.TransformerEncoderLayer(feature, num_heads)
+        print(self.attn)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        mask = torch.ones(H * W, H * W).triu().bool()
+        mask[0, 0] = False
+        return self.attn(src=x.reshape(B, C, -1).permute(2, 0, 1), src_mask=mask).permute(1, 2, 0).reshape(B, C, H, W)
+
+
+class NaivePixelCNN(nn.Module):
+    def __init__(self, feature,num_layer=5, activaton=nn.Hardswish(),optimizer=torch.optim.Adam,sm='-1_1', lossf=None):
+        super(NaivePixelCNN, self).__init__()
+        self.lossf = lossf
+        layer = []
+        layer.append(MaskedConv2D(3, feature, num_layer))
+        for i in range(5):
+            layer.append(MaskedConv2D(feature, feature, 5))
+            layer.append(nn.BatchNorm2d(feature))
+            layer.append(activaton)
+        layer.append(MaskedConv2D(feature, 256*3, 5))
+        self.layer = nn.ModuleList(layer)
+        self.optimizer=optimizer(self.layer.parameters())
+        self.sm = sm
+
+    def forward(self, x):
+        for l in self.layer:
+            x = l(x)
+        return x
+
+    def loss(self, img):
+        output = self.forward(img)
+        B,C,H,W=output.shape
+        if self.lossf is not None:
+            return self.lossf(output, img)
+        elif self.sm == '-1_1':
+            return F.cross_entropy(output.reshape(B,3,C//3,H,W).permute(0,2,1,3,4), ((img * 0.5 + 0.5) * 255).long()),(output.reshape(B,3,C//3,H,W).argmax(2)/255-0.5)/0.5
+    def batch(self,img,phase):
+        with torch.set_grad_enabled(phase=='train'):
+            loss,outimg=self.loss(img)
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            return {'loss':{'recon':loss.item()},'images':outimg}
+    def generate(self,size,device='cpu'):
+        return torch.zeros(32,3,size,size).to(device)
+        with torch.no_grad():
+            img=torch.zeros(1,3,size,size).to(device)
+            for i in range(size):
+                for j in range(size):
+                    output=self.forward(img)
+                    img[0,:,i,j]=F.softmax(output[0,:,i,j].reshape(3,256),dim=-1).multinomial(1).squeeze()
+            return img/255
+
+
+if __name__ == '__main__':
+    model = MaskedConv2D(1, 2, 5)
+    model = NaivePixelCNN(128)
+    print(model.generate(7).shape)
+    optimizer = torch.optim.Adam(model.parameters())
+    # B, C, H, W = 1, 3, 7, 7
+    # data = (torch.rand((B,C,H,W))-0.5)/0.5
+    # loss = model.loss(data)
+    # (loss).mean().backward()
+    # optimizer.step()
---
+++ model/train_pixelcnn.py
@@ -0,0 +1,154 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import torch
+from torchvision.utils import save_image
+
+torch.manual_seed(999)
+import json
+import pickle as pkl
+import torch.nn.functional as F
+import torch.nn as nn
+import utils.util as U
+from model.pixelcnn import NaivePixelCNN
+from utils.tfrecord import TFRDataloader
+from torch.utils.tensorboard import SummaryWriter
+import shutil
+
+
+def operate(phase):
+    if phase == 'train':
+        model.train()
+        loader = trainloader
+    else:
+        loader = valloader
+    loader.init()
+
+    mvci = U.MeanCoVariance_iter(device)
+
+    for img in loader:
+        iter_number[phase] += 1
+        B, C, H, W = img.shape
+        outstats = model.batch(img.to(device),phase=phase)
+        log = f'{iter_number[phase]},{outstats["loss"]}'
+        with open(logpath, 'a')as f:
+            f.write(log + '\n')
+        print(log)
+        writer.add_scalars('loss', outstats['loss'], iter_number[phase])
+
+        generatedimages = model.generate(args.size,device=device)
+        mvci.iter(inception(generatedimages.detach().to(device))[0])
+        # save_image(generatedimages, f'{savefolder}/gen_{iter_number[phase]}.jpg')
+        # save_image(outstats['images'], f'{savefolder}/recon_{iter_number[phase]}.jpg')
+        save_image(torch.cat([generatedimages[:B],outstats['images']],dim=2),f'{savefolder}/{iter_number[phase]}.jpg')
+    writer.add_images('recon_images', outstats['images'], iter_number[phase])
+    writer.add_images('gen_images', generatedimages, iter_number[phase])
+    # get FID
+    fid = U.fid(*inceptionstats[phase], *mvci.get(isbias=True))
+    # IS=cal_is(realimg)
+    writer.add_scalar(f'fid/{phase}', fid, iter_number[phase])
+    # Co.addvalue(writer,'IS',IS,e)
+    print(f'fid:{fid:.2f}')
+    U.savecloudpickle({
+        'model': model.to('cpu'),
+        'e': e,
+        'args': args,
+        'realstats': (realsigma, realmu),
+    }, savefolder + f'/chk.pth')
+    model.to(device)
+
+
+if __name__ == '__main__':
+    # try:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', default=32, type=int)
+    parser.add_argument('--model', default='ae')
+    parser.add_argument('--dataset', default='celeba')
+    parser.add_argument('--optimizer', default='adam')
+    parser.add_argument('--zsize', type=int, default=128)
+    parser.add_argument('--epoch', default=100, type=int)
+    parser.add_argument('--savefolder', default='tmp')
+    parser.add_argument('--size', default=64, type=int)
+    parser.add_argument('--reconloss', default='mse')
+    parser.add_argument('--feature', default=128, type=int)
+    parser.add_argument('--cpu', default=False, action='store_true')
+    parser.add_argument('--datasetpath', default='../data')
+    parser.add_argument('--debug', default=False, action='store_true')
+    parser.add_argument('--activation', default='relu')
+    parser.add_argument('--disable_zviz', default=True, action='store_true')
+    parser.add_argument('--KLD',default='Bernoulli')
+    parser.add_argument('--dicsize',default=512,type=int)
+    args = parser.parse_args()
+    epoch = args.epoch
+    device = 'cuda' if torch.cuda.is_available() and not args.cpu else 'cpu'
+    savefolder = 'data/' + args.savefolder
+    from gtmodel import InceptionV3
+
+    inception = InceptionV3([3]).to(device)
+    logpath = f'{savefolder}/log.txt'
+    shutil.rmtree(f'tfb/{args.savefolder}', ignore_errors=True)
+    shutil.rmtree(f'{savefolder}', ignore_errors=True)
+    writer = SummaryWriter(log_dir=f'tfb/{args.savefolder}')
+    writer.add_text('args', f'{args}')
+    os.makedirs(savefolder, exist_ok=True)
+
+    if args.activation == 'relu':
+        activation = nn.ReLU(inplace=True)
+    elif args.activation == 'hswish':
+        activation = nn.Hardswish(inplace=True)
+    elif args.activation == 'lrelu':
+        activation = nn.LeakyReLU(0.2, inplace=True)
+    if args.reconloss == 'mse':
+        reconloss = F.mse_loss
+    if args.optimizer == 'adam':
+        optimizer = torch.optim.Adam
+    if args.model == 'ae':
+        model = NaivePixelCNN(feature=args.feature,activaton=activation,optimizer=optimizer)
+
+    if args.dataset == 'celeba':
+        # loader = torch.utils.data.DataLoader(
+        #     CelebADataset(torchvision.datasets.CelebA(args.datasetpath, 'all', download=True), args.size, args.zsize,debug=args.debug),
+        #     batch_size=args.batchsize, num_workers=4, shuffle=True)
+        s,m=(0.5,0.5)
+        trainloader = TFRDataloader(path=args.datasetpath + '/celeba.tfrecord', epoch=1, batch=args.batchsize,
+                                    size=args.size, s=s,m=m, split='train')
+
+        valloader = TFRDataloader(path=args.datasetpath + '/celeba.tfrecord', epoch=1, batch=args.batchsize,
+                                  size=args.size, s=s,m=m, split='val')
+    inceptionstats = {}
+    for phase in ['train', 'val']:
+        realstatspath = f'__{args.dataset}_real_{phase}.pkl'
+        if os.path.exists(realstatspath):
+            print('load real stats')
+            with open(realstatspath, 'rb') as f:
+                realsigma, realmu = pkl.load(f)
+                realsigma = realsigma.to(device)
+                realmu = realmu.to(device)
+        else:
+            print('make real stats')
+            realsigma, realmu = U.make_gt_inception(inception, trainloader if phase == 'train' else valloader, device)
+            with open(realstatspath, 'wb') as f:
+                pkl.dump([realsigma.cpu(), realmu.cpu()], f)
+        inceptionstats[phase] = [realsigma, realmu]
+    with open(f'{savefolder}/args.json', 'w') as f:
+        json.dump(args.__dict__, f)
+
+    # if device == 'cuda':
+    #     model = torch.nn.DataParallel(model).to(device)
+    model=model.to(device)
+    iter_number = {'train': 0, 'val': 0}
+    # testinput = torch.randn(args.batchsize, 512, 2,2).to(device)
+    testinput=torch.randint(0, args.dicsize, [args.batchsize , 2 , 2]).to(device)
+    for e in range(epoch):
+        operate('train')
+        operate('val')
+    writer.close()
+
+# except:
+#     import traceback
+#     import sys
+#
+#     error_msg = traceback.format_exc()
+#     print(f'\033[31m{error_msg}\033[0m', file=sys.stderr)
