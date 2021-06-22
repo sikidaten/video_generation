fix update dic to use moving average

main.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     testinput=torch.randint(0, args.dicsize, [args.batchsize , args.size//4 , args.size//4]).to(device)
     for e in range(epoch):
         operate('train')
-        operate('val')
+        # operate('val')
     writer.close()
 
 # except:

model/vqvae.py
@@ -51,7 +51,7 @@ class ResBlock(nn.Module):
 
 
 class VQVAE(nn.Module):
-    def __init__(self, feature, z_feature, optimizer, activation, zdicsize=512):
+    def __init__(self, feature, z_feature, optimizer, activation, zdicsize=512,gamma=0.99):
         super(VQVAE, self).__init__()
         self.zdicsize = zdicsize
 
@@ -72,6 +72,10 @@ class VQVAE(nn.Module):
         self.optimizer = optimizer(self.parameters(),2e-4)
         self.z_dic = torch.randn(z_feature, self.zdicsize)
         self.vqdic = VQDic(self.z_dic)
+
+        self.N=0
+        self.m=torch.zeros(self.zdicsize)
+        self.gamma=gamma
 
     def forward(self, x):
         z = self.encoder(x)
@@ -100,6 +104,11 @@ class VQVAE(nn.Module):
         return self.decoder(vqz.to(randn.device))
 
     def update_dic(self, z, vqzidx):
+        B,C,H,W=z.shape
+        _N=self.N*self.gamma+B*(1-self.gamma)
+        self.N=_N
         for i in range(self.zdicsize):
             if (vqzidx == i).any():
-                self.z_dic[:, i] = z.permute(0, 2, 3, 1)[vqzidx == i].mean(0)
+                _mi=self.m[i]*self.gamma+z.permute(0, 2, 3, 1)[vqzidx == i].sum(0)*(1-self.gamma)
+                self.z_dic[:, i] = _mi/_N
+                self.m[i]=_mi

utils/tfrecord.py
@@ -52,7 +52,7 @@ class TFRDataloader():
         return (torch.from_numpy(next(self.tfdataset)) - self.m) / self.s
 
     def __len__(self):
-        return 202589 // self.batch
+        return 202589 // self.batch//4
     def init(self):
         self.__init__(path=self.path,epoch=self.epoch,batch=self.batch,s=self.s,m=self.m,split=self.split,size=self.size)
 