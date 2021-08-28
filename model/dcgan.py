e1e68414a1100fc22894aab5be75688e9f80e161
hokuseihal <hokuseihal@gmail.com>
2021-08-28 12:12:03 +0900

    fix init weight

--- model/dcgan.py
+++ model/dcgan.py
@@ -145,9 +145,10 @@ class DCGAN(nn.Module):
 
     def weights_init(self, m):
         classname = m.__class__.__name__
-        if classname.find('Conv2D') != -1:
+        print(classname)
+        if classname.find('Conv') != -1:
             nn.init.normal_(m.weight.data, 0.0, 0.02)
-        elif classname.find('BatchNorm2D') != -1:
+        elif classname.find('BatchNorm') != -1:
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0)
 
